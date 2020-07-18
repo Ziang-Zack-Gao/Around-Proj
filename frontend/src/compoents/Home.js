1f9b25adfe80d9c59f38ad1ceb03afe78f751132
Ziang (Zack) Gao <68089542+Ziang-Zack-Gao@users.noreply.github.com>
2020-07-18 17:11:39 -0400

    Add files via upload

--- frontend/src/compoents/Home.js
+++ frontend/src/compoents/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Tabs, Spin, Row, Col, Button} from 'antd';
+import {Tabs, Spin, Row, Col} from 'antd';
 import {
     GEO_OPTIONS,
     POS_KEY,
@@ -13,7 +13,7 @@ import {
 
 import Gallery from './Gallery';
 import CreatePostButton from './CreatePostButton';
-
+import AroundMap from './AroundMap';
 
 const {TabPane} = Tabs;
 
@@ -142,7 +142,15 @@ class Home extends React.Component {
                     {this.renderPosts(POST_TYPE_VIDEO)}
                 </TabPane>
                 <TabPane tab="Map" key="3">
-                    Content of tab 3
+                    <AroundMap
+                        googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyD3CEh9DXuyjozqptVB5LA-dN7MxWWkr9s&v=3.exp&libraries=geometry,drawing,places"
+                        loadingElement={<div style={{ height: `100%` }} />}
+                        containerElement={<div style={{ height: `600px` }} />}
+                        mapElement={<div style={{ height: `100%` }} />}
+                        posts={this.state.posts}
+                        loadNearbyPosts={this.loadNearbyPosts}
+                    />
+
                 </TabPane>
             </Tabs>
         );
